77cac9c1f7bc2b23183919a6f5b1391506900bc7
OHJ <osh2471@gmail.com>
2019-07-08 15:58:08 +0900

    First Commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    
+    articles = [
+        {
+            'id': 1, 
+            'title': 'Article one', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollitanim id est laborum.', 
+            'author':'Thorr', 
+            'create_date':'04-09-2018', 
+        },
+        {
+            'id': 2, 
+            'title': 'Article two', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            'author': 'Thorr Flask', 
+            'create_date': '05-09-2018', 
+        },
+        {
+            'id': 3, 
+            'title': 'Article three', 
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'author': 'Hansol CHoi', 
+            'create_date': '04-09-2018', 
+        }
+    ]
+    return articles
---
+++ test.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__' : 
+    print("Hello GaryKim")
+
+else :
+    print("Hello TaeKyoung")
